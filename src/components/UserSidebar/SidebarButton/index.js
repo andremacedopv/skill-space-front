e176eed53be154c0319c0aebaba363d57bb09e6b
Felipe <felipevaz444@gmail.com>
2022-08-20 15:14:08 -0300

    :sparkles: Creating sidebar componente

---
+++ src/components/UserSidebar/SidebarButton/index.js
@@ -0,0 +1,41 @@
+import { ButtonContainer, OptionsContainer } from "./styles"
+import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
+import { useState } from 'react'
+
+const SidebarButton = ({ children, Icon, options, collapse, setCollapse, ...props }) => {
+    const [expand, setExpand] = useState(false);
+
+    const expandButton = () => {
+        if (collapse) {
+            setExpand(true);
+        } else {
+            setExpand(!expand);
+        }
+        setCollapse(false);
+    }
+
+    return (
+        <>
+        <ButtonContainer {...props} expand={expand} collapse={collapse}> 
+            <div className="main-area" onClick={expandButton}>
+                <div className="main-btn">
+                    <Icon className='icon-btn'/>
+                    <div className="text-btn">
+                        {children} 
+                    </div>
+                </div>
+                {expand? <AiOutlineUp className="icon-arrow"/> : <AiOutlineDown className='icon-arrow'/>}
+            </div>
+        </ButtonContainer>
+        <OptionsContainer {...props} expand={expand} collapse={collapse}>
+            {options.map((option, index) => {
+                return (
+                    <a key={index} href={option.link} className="dashboard-link">{option.name}</a>
+                )
+            })}
+        </OptionsContainer>
+        </>
+    )
+}
+
+export default SidebarButton

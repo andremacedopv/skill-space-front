ed65ebf4bf2e38460898cd38b152b7d7ad99977d
Felipe <felipevaz444@gmail.com>
2022-08-20 21:04:40 -0300

    :lipstick: Styling first section

--- src/pages/EventIndexPage/index.js
+++ src/pages/EventIndexPage/index.js
@@ -1,8 +1,21 @@
 import { Container } from "./styles";
 
+import { AiOutlineCalendar } from "react-icons/ai"
+
 const EventIndexPage = () => {
   return (
     <Container>
+      
+      <div className="header">
+        <h1>Eventos</h1>
+        <button className="calendar-button">
+          <h2>Ver Calendário</h2>
+          <AiOutlineCalendar className="calendar-icon"/>
+        </button>
+      </div>
+
+      <h2>Meus convites</h2>
+      <h2>Próximos Eventos</h2>
       <p> ola! </p>
     </Container>
   )
--- src/pages/EventIndexPage/styles.js
+++ src/pages/EventIndexPage/styles.js
@@ -1,8 +1,51 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+  font-family: 'Roboto Flex';
+  padding: 5vh 3vw;
+
+  .header{
     display: flex;
-    width: 100%;
-    height: 100vh;
-    font-family: 'Roboto Flex';
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 3vh;
+
+    h1 {
+      color: #282424;
+      font-size: 40px;
+      font-weight: 500;
+    }
+
+    .calendar-button {
+      border: none;
+      background: none;
+      
+      display: flex;
+      align-items: center;
+      gap: 10px;
+
+      cursor: pointer;
+      color: #4A4A4A;
+      transition: 0.7s;
+
+      h2 {
+        font-size: 24px;
+        font-weight: 500;
+      }
+
+      .calendar-icon {
+        font-size: 35px;
+      }
+    }
+
+    .calendar-button:hover {
+      filter: brightness(200%);
+    }
+  }
+
 `;

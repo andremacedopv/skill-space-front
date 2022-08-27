4d158e02461766abaa7dd72ac6bc3c8855821527
Felipe <felipevaz444@gmail.com>
2022-08-27 15:53:38 -0300

    :lipstick: Modal speaker info

--- src/components/EventModal/index.js
+++ src/components/EventModal/index.js
@@ -1,5 +1,7 @@
 import { Container } from './styles'
 
+import speakerImg from '../../assets/userImg.jpg'
+
 import {AiOutlineClose} from 'react-icons/ai'
 import {MdLocationPin, MdOutlineLink} from 'react-icons/md'
 
@@ -7,7 +9,7 @@ const EventModal = ({event, setModal, ...props}) => {
 
   return (
     <Container> 
-      <div class="modal-box"> 
+      <div className="modal-box"> 
         <AiOutlineClose class="close-button" onClick={() => {setModal(false)}}/>
         <h1 className='modal-title'> Seminário da Inovação </h1>
         <h3 className='modal-date'> 23 de Setembro de 2021 </h3>
@@ -22,7 +24,15 @@ const EventModal = ({event, setModal, ...props}) => {
             <p> Ver no Mapa</p>
           </div>
         </div>
-        <p> Palestrante </p>
+        <p className='speaker-p'> Palestrante </p>
+        <div className='speaker-box'>
+          <img src={speakerImg} alt="user profile"></img>
+          <div className='speaker-text'>
+            <p className='speaker-name'> Pedro José </p>
+            <p className='speaker-job'> Empreendedor </p>
+            <p className='speaker-description'> Formado em Direito na UnB, lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.  </p>
+          </div>
+        </div>
       </div>
     </Container>
   )
--- src/components/EventModal/styles.js
+++ src/components/EventModal/styles.js
@@ -44,7 +44,7 @@ export const Container = styled.div`
   .modal-box {
     position: relative;
     background-color: #F8F9FA;
-    border-radius: 5px;
+    border-radius: 7px;
 
     padding: 20px;
 
@@ -118,6 +118,53 @@ export const Container = styled.div`
           font-size: 25px;
         }
       }
+    }
+
+    .speaker-p {
+      margin-top: 15px;
+      font-weight: 500;
+      font-size: 18px;
+      color: #5C5656;
+    }
+
+    .speaker-box {
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      border-radius: 19px;
+      padding: 20px 10px;
+
+      display: flex;
+      align-items: center;
+      gap: 20px;
+
+      img {
+        width: 100px;
+        height: 100px;
+        object-fit: cover;
+        border-radius: 999px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      }
+
+      .speaker-text {
+        .speaker-name {
+          font-weight: 500;
+          font-size: 22px;
+          color: #282323;
+        }
+  
+        .speaker-job {
+          font-weight: 500;
+          font-size: 16px;
+          color: #5C5656;
+        }
+  
+        .speaker-description {
+          margin-top: 5px;
+          font-weight: 400;
+          font-size: 18px;
+          color: #5C5656;
+        }
+      }
+
     }
 
   }

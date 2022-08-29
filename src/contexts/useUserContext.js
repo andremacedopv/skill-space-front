7147bed13fa98c8da6612391c17a660091c4ec83
Felipe <felipevaz444@gmail.com>
2022-08-29 18:03:01 -0300

    :sparkles: Context changes

--- src/contexts/useUserContext.js
+++ src/contexts/useUserContext.js
@@ -49,8 +49,8 @@ const UserProvider = ({children}) => {
             });
             console.log(response)
             if(response.data){
-                setUser({...response.data.user})
-                var encrypted = CryptoJS.AES.encrypt(JSON.stringify({...response.data.user}), process.env.REACT_APP_CRYPTO_KEY).toString();
+                setUser({...response.data})
+                var encrypted = CryptoJS.AES.encrypt(JSON.stringify({...response.data}), process.env.REACT_APP_CRYPTO_KEY).toString();
                 Cookies.set('user', encrypted, {expires: 1, sameSite: 'None', secure: true });
                 navigate('/')
                 api.defaults.headers.common['Authorization'] = response.data.token
